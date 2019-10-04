218493db29609182226a4fc843ce52207848efd2
Rafael Barreto <rafaelbarretoo2008@hotmail.com>
2019-10-04 09:02:25 -0300

    Implementation to configure new feature to conjured items, adjusts in update method and unit test.

---
+++ csharp/GildedRoseTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace csharp
+{
+    [TestFixture]
+    public class GildedRoseTest
+    {
+        [Test]
+        public void foo()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual("fixme", Items[0].Name);
+        }
+
+        [Test]
+        public void ConjureItemsTest()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Conjured Mana Cake", SellIn = 4, Quality = 2 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.IsTrue(Items[0].Quality == 0);
+
+        }
+    }
+}
